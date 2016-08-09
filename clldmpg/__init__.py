eb609d0faa3d4627a9ea0ea8ce81e4ffd3ed9fbe
Gereon Kaiping <anaphory@yahoo.de>
2016-08-09 12:33:25 +0200

    Adapt clld scaffold for LUCL use

--- clldmpg/__init__.py
+++ clldmpg/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.response import Response
+## Adapted for LUCL use by Gereon Kaiping
 
 from clld.interfaces import IOlacConfig
 from clld.web.views.olac import OlacConfig, Participant, Institution
@@ -6,14 +7,14 @@ from clld.web.views.olac import OlacConfig, Participant, Institution
 
 class MpgOlacConfig(OlacConfig):
     def admin(self, req):
-        return Participant("Admin", 'Robert Forkel', 'forkel@shh.mpg.de')
+        return Participant("Admin", 'Gereon Kaiping', 'g.a.kaiping@hum.leidenuniv.nl')
 
     def description(self, req):
         res = OlacConfig.description(self, req)
         res['institution'] = Institution(
-            'Max Planck Institute for the Science of Human History',
-            'http://shh.mpg.de',
-            'Jena, Germany')
+            'Leiden University Centre for Linguistics',
+            'http://hum.leiden.edu/lucl/',
+            'Leiden, The Netherlands')
         return res
 
 
@@ -21,7 +22,7 @@ def includeme(config):
     config.include('clld.web.app')
     config.registry.registerUtility(MpgOlacConfig(), IOlacConfig)
     config.add_static_view('clldmpg-static', 'clldmpg:static')
-    config.add_settings({'clld.publisher_logo': 'clldmpg:static/minerva.png'})
+    config.add_settings({'clld.publisher_logo': 'clldmpg:static/lucl.png'})
 
     config.add_route('google-site-verification', 'googlebbc8f4da1abdc58b.html')
     config.add_view(
